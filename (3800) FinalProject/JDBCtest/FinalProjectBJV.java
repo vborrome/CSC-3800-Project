c731c392c80866d1b4fb06c00ea141d92648268f
javongilchrist <49965014+javongilchrist@users.noreply.github.com>
2019-04-29 22:25:58 -0400

    Update FinalProjectBJV.java

--- (3800) FinalProject/JDBCtest/FinalProjectBJV.java
+++ (3800) FinalProject/JDBCtest/FinalProjectBJV.java
@@ -22,11 +22,11 @@ public class FinalProjectBJV {
 
 	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/movieTheater";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/MovieTheater";
 
 	// Database credentials
 	static final String USER = "root";
-	static final String PASS = "rootpassword";
+	static final String PASS = "Echo123jg";
 
 	public static void main(String[] args) {
 		Connection conn = null;
